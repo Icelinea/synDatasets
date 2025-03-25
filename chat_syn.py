2a99df41fc36236036de0edffff4f5be587fc6f8
iceline <2604818431@qq.com>
2025-03-25 18:06:47 +0800

    chatcode-draft-finished

--- chat_syn.py
+++ chat_syn.py
@@ -3,40 +3,66 @@ from utils.agent import Agent
 import random
 import json
 
+# path
+patientBackgroundPath = "data/PatientBackground/bg-example.json"
+patientPersonaPath = "data/Persona/patient.json"
+doctorPersonaPath = "data/Persona/doctor.json"
+outputPath = "./data/Chats/c1.json"
 
-# prompt
-patientPrompt = 1
-doctorPrompt = 2
 
+def chatdata_synthesis(epoches, maxChats):
+    # 加载
+    data = None
+    pbdata = None
+    ppdata = None
+    dpdata = None
 
-def chatdata_synthesis(epoches, maxChats, outputPath):
-    patient = Agent(globalModelPath, globalModelName, "patient")
-    doctor = Agent(globalModelPath, globalModelName, "doctor")
+    with open(patientBackgroundPath, 'r', encoding='utf-8') as f:
+        data = json.load(f)
+    pbdata = random.sample(data, 1)
 
-    # 具体 prompt?
-    patient.model_init(patientPrompt)
-    doctor.model_init(doctorPrompt)
+    with open(patientPersonaPath, 'r', encoding='utf-8') as f:
+        data = json.load(f)
+    ppdata = random.sample(data, 1)
 
-    reponses = []
+    with open(doctorPersonaPath, 'r', encoding='utf-8') as f:
+        data = json.load(f)
+    dpdata = random.sample(data, 1)
+
+    # prompt
+    patientSystemPrompt = "你是一位{}的希望向医生求助心理健康相关问题的患者，意味着{}。在与心理健康医生交流前，你的病患背景是：{}".format(ppdata['类型'], ppdata['解释'], pbdata['背景'])
+    doctorSystemPrompt = "你是一位{}的{}专业心理健康医生，在与患者交流时会一直保持着以下的问诊习惯：你的交流风格很{}，{}在恰当的时间和患者进行共情交流的习惯，{}向患者解释一些心理健康领域的专业术语。在交流前，你了解到患者的背景是：{}".format(dpdata["年龄"], dpdata["性别"], dpdata["语言风格"], dpdata["同理心"], dpdata["解释"], pbdata['背景'])
+    
+    patient = Agent(globalModelPath, globalModelName, "patient", pbdata, ppdata)
+    doctor = Agent(globalModelPath, globalModelName, "doctor", pbdata, dpdata)
+
+    patient.model_init(patientSystemPrompt)
+    doctor.model_init(doctorSystemPrompt)
+
+    chats = []
     for epoch in range(epoches):
-        chatNum = random.randint(1, maxChats)
-        chats = []
+        chatNum = random.randint(3, maxChats)
 
-        patientResponse = None
-        doctorResponse = None
-        
+        history = []
+        chats.append(pbdata)
+
         for i in range(chatNum):
             # 对话
-            patientResponse = patient.chat(doctorResponse, chats)
-            doctorResponse = doctor.chat(patientResponse, chats)
-            chats.append(
-                {"patient": patientResponse},
-                {"doctor": doctorResponse}
-            )
+            patientResponse = patient.chat(history)
+            history.append("患者：" + patientResponse)
+            doctorResponse = doctor.chat(history)
+            history.append("医生：" + doctorResponse)
 
-    # 输出包括患者背景相关信息(第一个) + 对话信息 ？
+            chats.append({"role": "user", "content": patientResponse})
+            if "EOF" in doctorResponse:
+                chats.append({"role": "assistant", "content": "医生认为当前患者的心理健康问题已得到改善"})
+                break
+            else:
+                chats.append({"role": "assistant", "content": doctorResponse})
+
+    # 输出包括患者背景相关信息
     with open(outputPath, 'w', encoding='utf-8') as o:
-        json.dump(reponses, o, ensure_ascii=False, indent=4)
+        json.dump(chats, o, ensure_ascii=False, indent=4)
 
 if __name__ == '__main__':
-    chatdata_synthesis(3, 3, "./data/Chats")
+    chatdata_synthesis(3, 5)
--- utils/agent.py
+++ utils/agent.py
@@ -12,7 +12,7 @@ class Agent():
         self.tokenizer = None
         
         self.role = role    # 索引名
-        self.background = background    # 背景
+        self.background = background    # 患者背景
         self.persona = persona  # 语言风格
         self.setupPrompt = None
 
@@ -80,8 +80,45 @@ class Agent():
         
         return response
     
-    def chat(self, input, history):
-        1
+    def chat(self, history):
+        # general
+        messages = []
+        messages.append(self.setupPrompt)
+
+        # role-specific
+        genPrompt = "你与对方的历史对话记录为：{}。其中每个对话前面都有表示其身份的名称医生或者患者。".format(history)
+        if self.role == "patient":
+            genPrompt += "如果没有历史对话记录，请你按照自己的患者求助背景和指定的性格向医生提出合理的问题。如果有历史对话记录，请你按照你自己的性格回答医生的问题或者继续自己的行为。请不要说谎，如实的说明你当前的想法，自身的心理健康问题是否得到改善或者恶化。注意字数需要控制在 10 到 100 个中文字数之间。注意使用一定要使用口语化的表达。"
+        elif self.role == "doctor":
+            genPrompt += "按照患者所提供的背景，当前的患者核心病症主题应该是{}。你作为心理健康领域医生需要坚持向患者探讨这个主题，尽量不要被带偏，直到患者的心理问题得到改善。你可以向患者提出问题来进一步了解患者的内心想法，也可以回答患者提出的问题。如果你认为当前主题已经探讨完全或者患者的心理问题得到了很大的改善，只需要回答\"EOF\"，不要有其他单词。注意字数需要控制在 10 到 100 个中文字数之间。注意使用一定要使用口语化的表达。".format(self.background["主题"])
+        assert genPrompt != ""
+
+        messages.append(
+            {"role": "user", "content": genPrompt}
+        )
+        
+        text = self.tokenizer.apply_chat_template(
+            messages,
+            tokenize=False,
+            add_generation_prompt=True
+        )
+        
+        with torch.no_grad():  # 禁用梯度计算
+            modelInputs = self.tokenizer([text], return_tensors="pt").to(device)
+            generatedIds = self.model.generate(
+                modelInputs.input_ids,
+                max_new_tokens=512
+            )
+            generatedIds = [
+                outputIds[len(input_ids):] for input_ids, outputIds in zip(modelInputs.input_ids, generatedIds)
+            ]
+            response = self.tokenizer.batch_decode(generatedIds, skip_special_tokens=True)[0]
+
+        # 释放 cuda 内存
+        del modelInputs, generatedIds
+        torch.cuda.empty_cache()
+        
+        return response
 
 
 if __name__ == '__main__':
